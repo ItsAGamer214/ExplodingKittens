19f8952044e1add81eaa60022f2e229b8d60e5e2
akhilk999 <awesomesetty99@gmail.com>
2024-04-24 15:29:29 -0500

    implementations for card & decks

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -1,4 +1,12 @@
 package model;
 
+import util.CardFactory;
+
 public class Deck {
+    CardFactory cardFactory;
+    int size;
+
+    public Deck(int size){
+        cardFactory = new CardFactory(size);
+    }
 }
