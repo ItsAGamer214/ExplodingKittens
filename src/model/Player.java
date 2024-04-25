8ef1af277836e712fc8746c48dbd53fa8e5b7203
Anish Reddy Alle <anish.alle11@gmail.com>
2024-04-25 14:52:47 -0500

    what the sigma

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,23 @@
 package model;
 
 public class Player {
+    private String name;
+    private Deck deck;
+    private static final int initNumCards = 7;
 
+
+    public Player(String name){
+        this.name = name;
+        deck = new Deck(initNumCards);
+
+    }
+
+    /** If an ExplodingKittenCard is drawn, then this function will return
+     *  true if they live, false if they die.
+     *
+     * @return
+     */
+    public boolean onExplode(){
+
+    }
 }
