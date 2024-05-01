c35a4f63af7de0a727c94522f28f8ed3e95e2b53
akhilk999 <awesomesetty99@gmail.com>
2024-05-01 15:17:48 -0500

    image sizing

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -19,52 +19,22 @@ public class GameController {
 
     public static void main(String[] args) throws IOException {
         GameView gameView = new GameView();
-        JDesktopPane desktopPane = new JDesktopPane();
-        gameView.add(desktopPane, BorderLayout.CENTER);
-        JInternalFrame gameFrame = new JInternalFrame("Game", true, false, false, true);
-        gameFrame.setBounds(0,0,gameView.getWidth(), gameView.getHeight()/2);
-        gameFrame.setVisible(true);
-        desktopPane.add(gameFrame);
-        //gets number of players
-        int numPlayers = Integer.parseInt(JOptionPane.showInputDialog(gameView,
-                "How many players?", 2));
-
-        //gets player names & inits them
+        int numPlayers = gameView.getNumPlayers();
         Deck deck = new Deck(numPlayers);
         List<Player> playerList = new ArrayList<>();
         List<PlayerView> playerViewList = new ArrayList<>();
-        String playerNames = JOptionPane.showInputDialog(gameView,
-                "Enter names:\n (Add comma between each name)",null);
-        String[] names = playerNames.split(",");
+        String[] names = gameView.getPlayerNames().split(",");
         for(int lcv = 0; lcv < names.length; lcv++){
             playerList.add(new Player(names[lcv].trim(), deck));
             playerViewList.add(new PlayerView(playerList.get(lcv)));
         }
 
-        //add deck image
-        BufferedImage deckImage = ImageIO.read(new File("src/images/deck.jpg"));
-        //deckImage = deckImage.getScaledInstance(-1, gameFrame.getHeight()/2, Image.SCALE_FAST);
-        int newWidth = resize(deckImage.getHeight(), gameFrame.getHeight()/2, deckImage.getWidth());
-        BufferedImage newImage = new BufferedImage(newWidth,gameFrame.getHeight()/2 , BufferedImage.TYPE_INT_RGB);
-
-        Graphics g = newImage.createGraphics();
-        g.drawImage(deckImage, 0, 0,newWidth , gameFrame.getHeight()/2, null);
-        g.dispose();
-
-
-        JLabel deckLabel = new JLabel(new ImageIcon(newImage));
-        JPanel panel = new JPanel();
-        panel.add(deckLabel);
         Image lastCardImage = ImageIO.read(new File("src/images/defuse/Laser-Pointer.jpg"));
-        lastCardImage = lastCardImage.getScaledInstance(-1, gameFrame.getHeight()*2/3, Image.SCALE_FAST);
-        JLabel lastCardLabel = new JLabel(new ImageIcon(lastCardImage));
-        panel.add(lastCardLabel);
-        gameFrame.setLayout(new BorderLayout());
-        gameFrame.add(panel, BorderLayout.CENTER);
-    }
-
-    public static int resize(int old1, int new1, int old2){
-        double proportion = new1*1.0/old1;
-        return (int) (old2*proportion);
+        lastCardImage = lastCardImage.getScaledInstance(-1, gameView.gameFrame.getHeight()/2, Image.SCALE_DEFAULT);
+        JLabel lastCardLabel = new JLabel(new ImageIcon(lastCardImage), SwingConstants.CENTER);
+        gameView.gamePanel.add(lastCardLabel, BorderLayout.CENTER);
+        gameView.gameFrame.setLayout(new BorderLayout());
+        gameView.gameFrame.add(gameView.gamePanel, BorderLayout.CENTER);
+        gameView.gameFrame.setVisible(true);
     }
 }
--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,14 +1,40 @@
 package view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class GameView extends JFrame {
 
-    public GameView(){
+    public JDesktopPane desktopPane;
+    public JInternalFrame gameFrame;
+    public JPanel gamePanel;
+    public GameView() throws IOException {
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setVisible(true);
         this.setResizable(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Exploding Kittens");
+        desktopPane = new JDesktopPane();
+        add(desktopPane, BorderLayout.CENTER);
+        gameFrame = new JInternalFrame("Game", true, false, false, true);
+        gameFrame.setBounds(0,0,this.getWidth(), this.getHeight()/2);
+        desktopPane.add(gameFrame);
+        Image deckImage = ImageIO.read(new File("src/images/deck.jpg"));
+        deckImage = deckImage.getScaledInstance(-1, gameFrame.getHeight()/2, Image.SCALE_DEFAULT);
+        JLabel deckLabel = new JLabel(new ImageIcon(deckImage));
+        gamePanel = new JPanel();
+        gamePanel.add(deckLabel);
+    }
+
+    public int getNumPlayers(){
+        return Integer.parseInt(JOptionPane.showInputDialog(this,
+                "How many players?", 2));
+    }
+    public String getPlayerNames(){
+        return JOptionPane.showInputDialog(this,
+                "Enter names:\n (Add comma between each name)",null);
     }
 }
